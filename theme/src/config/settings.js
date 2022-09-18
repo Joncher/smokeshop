0a077d4a8baf5bdbdc74e21ca42d83ae23f3f902
John Crespi Oncher <johnoncher@gmail.com>
2022-09-18 08:32:41 -0400

    updated photo

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Woodhaven Smoke Shop`,
 	description: `Best Smoke Shop in NYC`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	url: `https://woodhaven-smokeshop.netlify.app`, // No trailing slash!
 	name: `Woodhaven Smoke Shop`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
 };
